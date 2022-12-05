185dac2e56d53385b82a890301a489a8e1a42578
Apenmun <apenmun@gmail.com>
2022-12-05 13:22:06 +0100

    <first section done>

--- cart/src/App.vue
+++ cart/src/App.vue
@@ -47,24 +47,26 @@
 
 
   
-  <div class="grid grid-cols-3 gap-4 justify-items-auto p-2 bg-gray-200">
+  <div class="grid grid-cols-3 gap-4 justify-items-center p-2 bg-gray-100">
     <div
       class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
       <a href="#">
         <img
           class="p-8 rounded-t-lg"
-          src="/docs/images/products/apple-watch.png"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/132381/3/132381386G1.png"
           alt="product image"
         />
       </a>
       <div class="px-5 pb-5">
-        <a href="#">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+          Notebook Lenovo Ideapad 320 Intel® Core i5-7200u 8GB
+          </h5></a>
+
+  
+
+
         <div class="flex items-center justify-between">
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
@@ -77,9 +79,6 @@
           >
         </div>
         <div class="flex flex-col items-center justify-between">
-          <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
-          >
           <a
             href="#"
             class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
@@ -98,28 +97,24 @@
         />
       </a>
       <div class="px-5 pb-5">
-        <a href="#">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+          Notebook Dell Inspiron i15-3567-A30P Intel Core 7ª i5 4GB
+          </h5></a>
         <div class="flex items-center justify-between">
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >R$ 2259, 00</span
+            >R$ 2284, 00</span
           >
 
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >10 x 226, 00</span
+            >10 x 228, 00</span
           >
         </div>
         <div class="flex flex-col items-center justify-between">
-          <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
-          >
+          
           <a
             href="#"
             class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
@@ -133,33 +128,29 @@
       <a href="#">
         <img
           class="p-8 rounded-t-lg"
-          src="/docs/images/products/apple-watch.png"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/132165/8/132165801G1.jpg"
           alt="product image"
         />
       </a>
       <div class="px-5 pb-5">
-        <a href="#">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+          Notebook Samsung Essentials E21 Intel Celeron Dual Core
+          </h5></a>
         <div class="flex items-center justify-between">
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >R$ 2259, 00</span
+            >R$ 1490, 00</span
           >
 
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >10 x 226, 00</span
+            >10 x 149, 00</span
           >
         </div>
         <div class="flex flex-col items-center justify-between">
-          <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
-          >
+          
           <a
             href="#"
             class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
@@ -173,33 +164,28 @@
       <a href="#">
         <img
           class="p-8 rounded-t-lg"
-          src="/docs/images/products/apple-watch.png"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/132260/6/132260681G1.jpg"
           alt="product image"
         />
       </a>
       <div class="px-5 pb-5">
-        <a href="#">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+          Notebook Samsung Expert X22 Intel Core 7 i5 8GB
+          </h5></a>
         <div class="flex items-center justify-between">
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >R$ 2259, 00</span
+            >R$ 2307, 00</span
           >
 
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >10 x 226, 00</span
+            >10 x 231, 00</span
           >
         </div>
         <div class="flex flex-col items-center justify-between">
-          <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
-          >
           <a
             href="#"
             class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
@@ -213,33 +199,28 @@
       <a href="#">
         <img
           class="p-8 rounded-t-lg"
-          src="/docs/images/products/apple-watch.png"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/133252/7/133252789G1.jpg"
           alt="product image"
         />
       </a>
       <div class="px-5 pb-5">
-        <a href="#">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+          Notebook VAIO Fit 15S B1211B Intel Core i5 4GB
+          </h5></a>
         <div class="flex items-center justify-between">
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >R$ 2259, 00</span
+            >R$ 2599, 00</span
           >
 
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >10 x 226, 00</span
+            >10 x 260, 00</span
           >
         </div>
         <div class="flex flex-col items-center justify-between">
-          <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
-          >
           <a
             href="#"
             class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
@@ -253,33 +234,238 @@
       <a href="#">
         <img
           class="p-8 rounded-t-lg"
-          src="/docs/images/products/apple-watch.png"
+          src="https://images-submarino.b2w.io/produtos/01/00/sku/34470/9/34470934G1.jpg"
           alt="product image"
         />
       </a>
       <div class="px-5 pb-5">
-        <a href="#">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
           <h5
-            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
           >
-            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-          </h5>
-        </a>
+          Notebook Dell Alienware - i7 16GB
+          </h5></a>
         <div class="flex items-center justify-between">
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >R$ 2259, 00</span
+            >R$ 14000, 00</span
           >
 
           <span
             class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
-            >10 x 226, 00</span
+            >10 x 1400, 00</span
           >
         </div>
         <div class="flex flex-col items-center justify-between">
-          <span class="text-3xl font-bold text-gray-900 dark:text-white"
-            >$599</span
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
           >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="https://images-americanas.b2w.io/produtos/01/00/sku/29296/2/29296259G1.jpg"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5
+          >
+          Smartphone Xiaomi Mi A1 dual Android one 7.1
+          </h5></a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 1199, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 120, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/132474/0/132474081G1.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5
+          >
+          Smartphone Moto G 5S Dual Chip Android 7.0
+          </h5></a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 929, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 93, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/132651/7/132651745G1.jpg"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5
+          >
+          iPhone 8 Dourado 64GB Tela 4.7" IOS 11
+          </h5></a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 3949, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 395, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/125911/8/125911828G1.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5
+          >
+          Smartphone Samsung Galaxy S7 Edge
+          </h5></a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 1943, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 194, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="https://images-americanas.b2w.io/produtos/01/00/item/133453/1/133453185G1.jpg"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5
+          >
+          Smartphone Motorola Moto G6 Plus
+          </h5></a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 1699, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 170, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="https://images-submarino.b2w.io/produtos/01/00/item/133666/1/133666164G1.jpg"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#" class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+          <h5
+          >
+          Smartphone Motorola Moto Z3 Play
+          </h5></a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2999, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 300, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
           <a
             href="#"
             class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
