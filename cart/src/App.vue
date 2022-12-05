38ceeb7680d6f2eac20e80a8b54376f82411194b
Apenmun <apenmun@gmail.com>
2022-12-05 12:22:35 +0100

    <second commit>

---
+++ cart/src/App.vue
@@ -0,0 +1,300 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-gray-900 p-6">
+    <div class="block lg:hidden">
+      <button
+        class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
+      >
+        <svg
+          class="fill-current h-3 w-3"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <title>Menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+      </button>
+    </div>
+    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+      <div class="text-sm lg:flex-grow">
+        <a
+          href="#responsive-header"
+          class="block mt-4 lg:inline-block lg:mt-0 text-gray-100 hover:text-white mr-4"
+        >
+          AllProducts
+        </a>
+        <a
+          href="#responsive-header"
+          class="block mt-4 lg:inline-block lg:mt-0 text-gray-100 hover:text-white mr-4"
+        >
+          Smartphones
+        </a>
+        <a
+          href="#responsive-header"
+          class="block mt-4 lg:inline-block lg:mt-0 text-gray-100 hover:text-white"
+        >
+          Notebooks
+        </a>
+      </div>
+      <div>
+        <a
+          href="#"
+          class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-blue-400 hover:border-transparent hover:text-teal-500 bg-blue-400 hover:bg-white mt-4 lg:mt-0"
+          >Cart</a
+        >
+      </div>
+    </div>
+  </nav>
+
+
+  
+  <div class="grid grid-cols-3 gap-4 justify-items-auto p-2 bg-gray-200">
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="/docs/images/products/apple-watch.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          </h5>
+        </a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2259, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 226, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white"
+            >$599</span
+          >
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="/docs/images/products/apple-watch.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          </h5>
+        </a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2259, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 226, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white"
+            >$599</span
+          >
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="/docs/images/products/apple-watch.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          </h5>
+        </a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2259, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 226, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white"
+            >$599</span
+          >
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="/docs/images/products/apple-watch.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          </h5>
+        </a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2259, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 226, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white"
+            >$599</span
+          >
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="/docs/images/products/apple-watch.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          </h5>
+        </a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2259, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 226, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white"
+            >$599</span
+          >
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+    <div
+      class="w-full max-w-sm bg-white rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img
+          class="p-8 rounded-t-lg"
+          src="/docs/images/products/apple-watch.png"
+          alt="product image"
+        />
+      </a>
+      <div class="px-5 pb-5">
+        <a href="#">
+          <h5
+            class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white"
+          >
+            Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+          </h5>
+        </a>
+        <div class="flex items-center justify-between">
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >R$ 2259, 00</span
+          >
+
+          <span
+            class="text-gray-900 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3 mb-3"
+            >10 x 226, 00</span
+          >
+        </div>
+        <div class="flex flex-col items-center justify-between">
+          <span class="text-3xl font-bold text-gray-900 dark:text-white"
+            >$599</span
+          >
+          <a
+            href="#"
+            class="text-white bg-green-500 hover:bg-green-900 focus:ring-4 focus:outline-none focus:bg-green-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-900 dark:hover:bg-green-900 dark:focus:bg-green-900"
+            >Add to cart</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+
+ 
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
